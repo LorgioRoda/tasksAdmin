28536bb590e7c9af2ab3a198651d098fb074ef84
Lorgio Roda <lorgiorodaroca@gmail.com>
2022-07-23 15:41:06 +0200

    working on test

--- task_management/src/services/test/task.test.ts
+++ task_management/src/services/test/task.test.ts
@@ -1,14 +1,16 @@
 import { getTasks } from "../tasks";
-import axios from 'axios';
+import axios from "axios";
 
-jest.mock('axios');
+jest.mock("axios");
 
-test("should fetch taks", () => {
-  const task = [{id: 1, note: 'test'}];
-  const resp = {data: task};
-  // @ts-ignore
-  axios.get.mockResolvedValue(resp);
-  return getTasks().then((response) => {
-    expect(response.data).toEqual(task);
+describe("Fetch API", () => {
+  test("should fetch task", () => {
+    const task = [{ id: 1, note: "test" }];
+    const resp = { data: task };
+    // @ts-ignore
+    axios.get.mockResolvedValue(resp);
+    return getTasks().then((response) => {
+      expect(response.data).toEqual(task);
+    });
   });
 });
